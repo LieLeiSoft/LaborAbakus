bb80b57ce902a9cd4ffd1c631dd8b6bed824b1e0
Ingo <ingo.leisner@gmail.com>
2017-09-19 17:36:27 +0200

    neues Layout "Berechnung der relativen Standardabweichung" neue Activity "Berechnung der relativen Standardabweichung" jetzt funktioniert es!!!  :-)

--- app/src/main/java/de/laborabakus/RSD_Activity.java
+++ app/src/main/java/de/laborabakus/RSD_Activity.java
@@ -1,12 +1,14 @@
 package de.laborabakus;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -33,6 +35,13 @@ public class RSD_Activity extends Activity /*implements OnFocusChangeListener */
 
     	// Activity registrieren, damit sie später an zentraler Stelle (Hauptmenue) geschlossen werden kann
 	    ActivityRegistry.register(this);
+
+        EditText et = null;
+        // Cursor in erstes Eingabefeld setzen und numerische Tastatur einschalten
+        et = (EditText) findViewById(R.id.Eingabewert);
+        et.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(et, InputMethodManager.SHOW_FORCED);
 	} // onCreate
 
 	/** wird ausgeführt, wenn Activicty angezeigt wird */
@@ -56,60 +65,82 @@ public class RSD_Activity extends Activity /*implements OnFocusChangeListener */
         EditText et;
         TextView tv;
         String Eingabetext;
+        String strAusgabetext;
 
 
         // *********** Eingabewert wird ausgelesen *************
-
         et = (EditText) findViewById(R.id.Eingabewert);
         Eingabetext = et.getText().toString();
 
-        // *********** Eingabewert wird in Zahl umgewandelt *************
+        // *********** Weiter nur bei Eingabe *************
+        if (Eingabetext.equals("") == false)
+        {
+            // *********** Eingabewert wird in Zahl umgewandelt *************
+            dblAktuellerWert = Double.parseDouble(Eingabetext);
 
-        dblAktuellerWert = Double.parseDouble(Eingabetext);
+            // *********** RSD wird berechnet *************
+            x = x + dblAktuellerWert;
+            n = n + 1;
+            X = x / n;
+            d = dblAktuellerWert - X;
+            d = Math.pow(d, 2);   //d hoch 2
+            Sd = Sd + d;
+            s = Math.sqrt(Sd / (n - 1));
+            RSD = (s * 100 / X);
 
-        // *********** RSD wird berechnet *************
+            // *********** Ausgabe Anzahl Meßwerte *************
+            strAusgabetext = ActivityTools.fktDoubleToStringFormat(n, 0); // 0 Nachkommastellen
+            tv = (TextView) findViewById(R.id.tvAnzahl_Messwert);
+            tv.setText(strAusgabetext);
 
-        x = x + dblAktuellerWert;
-        n = n + 1;
-        X = x/n;
-        d = dblAktuellerWert - X;
-        d = Math.pow(d, 2);   //d hoch 2
-        Sd = Sd + d;
-        s = Math.sqrt (Sd/(n-1));
-        RSD =(s*100/X);
+            // *********** Ausgabe Mittelwert *************
+            strAusgabetext = ActivityTools.fktDoubleToStringFormat(X, 4); // 4 Nachkommastellen
+            tv = (TextView) findViewById(R.id.tv_Mittelwert);
+            tv.setText(strAusgabetext);
 
-        // *********** Ausgabe Anzahl Meßwerte *************
+            // *********** Ausgabe Standardabweichung *************
+            strAusgabetext = ActivityTools.fktDoubleToStringFormat(s, 4); // 4 Nachkommastellen
+            tv = (TextView) findViewById(R.id.tvStandardabweichung);
+            tv.setText(strAusgabetext);
 
-        tv = (TextView) findViewById(R.id.tvAnzahl_Messwert);
-        tv.setText(Double.toString(n));
+            // *********** Ausgabe relative Standardabweichung % *************
+            strAusgabetext = ActivityTools.fktDoubleToStringFormat(RSD, 4); // 4 Nachkommastellen
+            tv = (TextView) findViewById(R.id.tvRSD);
+            tv.setText(strAusgabetext);
 
-        // *********** Ausgabe Mittelwert *************
-
-        tv = (TextView) findViewById(R.id.tv_Mittelwert);
-        tv.setText(Double.toString(X));
-
-        // *********** Ausgabe Standardabweichung *************
-
-        tv = (TextView) findViewById(R.id.tvStandardabweichung);
-        tv.setText(Double.toString(s));
-
-        // *********** Ausgabe relative Standardabweichung % *************
-
-        tv = (TextView) findViewById(R.id.tvStandardabweichung);
-        tv.setText(Double.toString(RSD));
+            // *********** Eingabefeld für nächste Eingabe leeren *************
+            et = (EditText) findViewById(R.id.Eingabewert);
+            et.setText("");
+        }
 
     } // btnWeiter
-
     // Eingabefelder zurücksetzen
     public void btnClear(View v)
     {
     	EditText et;
-
         et = (EditText) findViewById(R.id.Eingabewert);
         et.setText("");
-    }
-	
-} // btnClear
+        TextView tv;
+        tv = (TextView) findViewById(R.id.tvAnzahl_Messwert);
+        tv.setText("");
+        tv = (TextView) findViewById(R.id.tv_Mittelwert);
+        tv.setText("");
+        tv = (TextView) findViewById(R.id.tvStandardabweichung);
+        tv.setText("");
+        tv = (TextView) findViewById(R.id.tvRSD);
+        tv.setText("");
+
+        x = 0;
+        n = 0;
+        X = 0;
+        d = 0;
+        Sd = 0;
+        s = 0;
+        RSD = 0;
+
+
+
+    } // btnClear
 	
 	/********************************************
 	 ************** Menue Button ****************

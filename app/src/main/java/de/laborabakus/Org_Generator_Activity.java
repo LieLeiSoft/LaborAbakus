81af8e22f103ba63d3cf37dce4900b037bcfe02d
Frank <LieLei.Soft@gmail.com>
2019-11-24 00:12:55 +0100

    Strukturformel-Editor: die Bildnamen (z.B. "hh1000a1_008") werden jetzt zentral in "Org_Strukturformeln_Activity.java" verwaltet. Dadurch ist keine unübersichtliche Case-Anweisung in "Org_Generator_Activity.java" mehr nötig.

--- app/src/main/java/de/laborabakus/Org_Generator_Activity.java
+++ app/src/main/java/de/laborabakus/Org_Generator_Activity.java
@@ -38,83 +38,22 @@ public class Org_Generator_Activity extends Activity {
     {
         super.onResume();
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-
-        strStrukturformel = prefs.getString("Strukturformel", "");
-
         if(resIdFeld != 0) // Wird abgefangen, wenn beim ersten Mal resID =0
         {
             ImageButton ibt;
+
+            // In der Activity "Org_Strukturformeln_Activity.java" werden in der Methode "btnZelle"
+            // die Schlüssel "Strukturformel-Zellenname" und "Strukturformel-ResId" gesetzt.
+            // Die Werte sind abhängig von dem Bild, das man im Layout "org_strukturformeln.xml" angetippt hat.
+            // Diese Schlüssel können nun hier gelesen / ausgewertet werden
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+            int intResId = prefs.getInt("Strukturformel-ResId", 0);
+
             ibt = (ImageButton) findViewById(resIdFeld);
 
-            strZelle = strStrukturformel.substring(9,12);   // Beispiel: ibtZelle_115 => 115
-
-            intZelle = Integer.parseInt(strZelle);
-
-            String strBildname = "hh0001a1_008";
-            int intBildId = 4711;
-
-            switch (intZelle)
-            {
-                case 11: ibt.setImageResource(R.drawable.co1111a12_011); break;
-                case 13: ibt.setImageResource(R.drawable.hh1000a1_008); break;
-                case 14: ibt.setImageResource(R.drawable.hh0100a1_008); break;
-                case 15: ibt.setImageResource(R.drawable.hh0010a1_008); break;
-
-//                case 16: ibt.setImageResource(R.drawable.hh0001a1_008); break;
-                case 16: ibt.setImageResource(intBildId); break;
-
-                case 21: ibt.setImageResource(R.drawable.an1101a13_019); break;
-                case 22: ibt.setImageResource(R.drawable.an1110a13_019); break;
-                case 23: ibt.setImageResource(R.drawable.an0111a13_019); break;
-                case 24: ibt.setImageResource(R.drawable.an1011a13_019); break;
-                case 25: ibt.setImageResource(R.drawable.an0101a14_027); break;
-                case 26: ibt.setImageResource(R.drawable.an1010a14_027); break;
-                case 31: ibt.setImageResource(R.drawable.an1000a15_035); break;
-                case 32: ibt.setImageResource(R.drawable.an0100a15_035); break;
-                case 33: ibt.setImageResource(R.drawable.an0010a15_035); break;
-                case 34: ibt.setImageResource(R.drawable.an0001a15_035); break;
-                case 35: ibt.setImageResource(R.drawable.an0101a28_054); break;
-                case 36: ibt.setImageResource(R.drawable.an1010a28_054); break;
-                case 41: ibt.setImageResource(R.drawable.an0101a42_081); break;
-                case 42: ibt.setImageResource(R.drawable.an1010a42_081); break;
-                case 43: ibt.setImageResource(R.drawable.an0101a56_108); break;
-                case 44: ibt.setImageResource(R.drawable.an1010a56_108); break;
-                case 45: ibt.setImageResource(R.drawable.an0101a70_135); break;
-                case 46: ibt.setImageResource(R.drawable.an1010a70_135); break;
-                case 51: ibt.setImageResource(R.drawable.an0101a84_162); break;
-                case 52: ibt.setImageResource(R.drawable.an1010a84_162); break;
-                case 53: ibt.setImageResource(R.drawable.an0101a98_189); break;
-                case 54: ibt.setImageResource(R.drawable.an1010a98_189); break;
-                case 55: ibt.setImageResource(R.drawable.an0101a112_216); break;
-                case 56: ibt.setImageResource(R.drawable.an1010a112_216); break;
-                case 61: ibt.setImageResource(R.drawable.an0101a126_243); break;
-                case 62: ibt.setImageResource(R.drawable.an1010a126_243); break;
-                case 63: ibt.setImageResource(R.drawable.an0101a140_27); break;
-                case 64: ibt.setImageResource(R.drawable.an1010a140_27); break;
-
-                case 71: ibt.setImageResource(R.drawable.en2101a12_011); break;
-                case 72: ibt.setImageResource(R.drawable.en1210a12_011); break;
-                case 73: ibt.setImageResource(R.drawable.en0121a12_011); break;
-                case 74: ibt.setImageResource(R.drawable.en1012a12_011); break;
-                case 75: ibt.setImageResource(R.drawable.en2020a12_011); break;
-                case 76: ibt.setImageResource(R.drawable.en0202a12_011); break;
-                case 81: ibt.setImageResource(R.drawable.en2110a24_022); break;
-                case 82: ibt.setImageResource(R.drawable.en2011a24_022); break;
-                case 83: ibt.setImageResource(R.drawable.en1120a24_022); break;
-                case 84: ibt.setImageResource(R.drawable.en1021a24_022); break;
-                case 86: ibt.setImageResource(R.drawable.en1111a24_022); break;
-                case 91: ibt.setImageResource(R.drawable.en0211a24_022); break;
-                case 92: ibt.setImageResource(R.drawable.en1201a24_022); break;
-                case 93: ibt.setImageResource(R.drawable.en1102a24_022); break;
-                case 94: ibt.setImageResource(R.drawable.en0112a24_022); break;
-                case 96: ibt.setImageResource(R.drawable.en1111b24_022); break;
-                case 101: ibt.setImageResource(R.drawable.in1010a24_022); break;
-                case 102: ibt.setImageResource(R.drawable.in0101a24_022); break;
-            }
+            ibt.setImageResource(intResId);
         }
-
-
     } // onResume
 
 
--- app/src/main/java/de/laborabakus/Org_Strukturformeln_Activity.java
+++ app/src/main/java/de/laborabakus/Org_Strukturformeln_Activity.java
@@ -9,15 +9,10 @@ import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.ImageButton;
-//import android.support.v4.content.ContextCompat;
-
 
 public class Org_Strukturformeln_Activity extends Activity {
 
-
-    String strZellenname;
-
-	@Override
+    @Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 	    super.onCreate(savedInstanceState);
@@ -45,38 +40,254 @@ public class Org_Strukturformeln_Activity extends Activity {
 
     public void btnZelle(View v)
     {
-        int intResId;
+        String strZellenname = "";
+        int intResId = 0;
 
         ImageButton btn = (ImageButton) findViewById(v.getId());
         Drawable drawable = btn.getDrawable();
 /*
-
 A) drawables with theme attributes
-
-ContextCompat.getDrawable(getActivity(), R.drawable.name);
-
-You’ll obtain a styled Drawable as your Activity theme instructs. This is probably what you need.
+   ContextCompat.getDrawable(getActivity(), R.drawable.name);
+   You’ll obtain a styled Drawable as your Activity theme instructs. This is probably what you need.
 
 B) drawables without theme attributes
-
-ResourcesCompat.getDrawable(getResources(), R.drawable.name, null);
-
-You’ll get your unstyled drawable the old way.
+   ResourcesCompat.getDrawable(getResources(), R.drawable.name, null);
+   You’ll get your unstyled drawable the old way.
  */
 
 //      if (drawable.getConstantState().equals(getResources().getDrawable(R.drawable.hh0001a1_008).getConstantState())){
-// "getResources().getDrawable" ist seit Android 5.1 (API 22) veraltet (deprecated)
-        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.hh0001a1_008).getConstantState())){
-            strZellenname = "hh0001a1_008";
-            intResId = R.drawable.hh0001a1_008;
+//      "getResources().getDrawable" ist seit Android 5.1 (API 22) veraltet (deprecated)
+
+        // ***********
+        // Kohlenstoff
+        // ***********
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.co1111a12_011).getConstantState())){
+            strZellenname =           "co1111a12_011";
+            intResId      = R.drawable.co1111a12_011;
         }
 
-        strZellenname = getResources().getResourceEntryName(v.getId());  // Der ID Zellenname wird in einen String umgewandelt
+        // ***********
+        // Wasserstoff
+        // ***********
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.hh1000a1_008).getConstantState())){
+            strZellenname =           "hh1000a1_008";
+            intResId      = R.drawable.hh1000a1_008;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.hh0100a1_008).getConstantState())){
+            strZellenname =           "hh0100a1_008";
+            intResId      = R.drawable.hh0100a1_008;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.hh0010a1_008).getConstantState())){
+            strZellenname =           "hh0010a1_008";
+            intResId      = R.drawable.hh0010a1_008;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.hh0001a1_008).getConstantState())){
+            strZellenname =           "hh0001a1_008";
+            intResId      = R.drawable.hh0001a1_008;
+        }
+
+        // ****
+        // "en"
+        // ****
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1101a13_019).getConstantState())){
+            strZellenname =           "an1101a13_019";
+            intResId      = R.drawable.an1101a13_019;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1110a13_019).getConstantState())){
+            strZellenname =           "an1110a13_019";
+            intResId      = R.drawable.an1110a13_019;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0111a13_019).getConstantState())){
+            strZellenname =           "an0111a13_019";
+            intResId      = R.drawable.an0111a13_019;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1011a13_019).getConstantState())){
+            strZellenname =           "an1011a13_019";
+            intResId      = R.drawable.an1011a13_019;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a14_027).getConstantState())){
+            strZellenname =           "an0101a14_027";
+            intResId      = R.drawable.an0101a14_027;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a14_027).getConstantState())){
+            strZellenname =           "an1010a14_027";
+            intResId      = R.drawable.an1010a14_027;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1000a15_035).getConstantState())){
+            strZellenname =           "an1000a15_035";
+            intResId      = R.drawable.an1000a15_035;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0100a15_035).getConstantState())){
+            strZellenname =           "an0100a15_035";
+            intResId      = R.drawable.an0100a15_035;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0010a15_035).getConstantState())){
+            strZellenname =           "an0010a15_035";
+            intResId      = R.drawable.an0010a15_035;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0001a15_035).getConstantState())){
+            strZellenname =           "an0001a15_035";
+            intResId      = R.drawable.an0001a15_035;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a28_054).getConstantState())){
+            strZellenname =           "an0101a28_054";
+            intResId      = R.drawable.an0101a28_054;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a28_054).getConstantState())){
+            strZellenname =           "an1010a28_054";
+            intResId      = R.drawable.an1010a28_054;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a42_081).getConstantState())){
+            strZellenname =           "an0101a42_081";
+            intResId      = R.drawable.an0101a42_081;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a42_081).getConstantState())){
+            strZellenname =           "an1010a42_081";
+            intResId      = R.drawable.an1010a42_081;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a56_108).getConstantState())){
+            strZellenname =           "an0101a56_108";
+            intResId      = R.drawable.an0101a56_108;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a56_108).getConstantState())){
+            strZellenname =           "an1010a56_108";
+            intResId      = R.drawable.an1010a56_108;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a70_135).getConstantState())){
+            strZellenname =           "an0101a70_135";
+            intResId      = R.drawable.an0101a70_135;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a70_135).getConstantState())){
+            strZellenname =           "an1010a70_135";
+            intResId      = R.drawable.an1010a70_135;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a84_162).getConstantState())){
+            strZellenname =           "an0101a84_162";
+            intResId      = R.drawable.an0101a84_162;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a84_162).getConstantState())){
+            strZellenname =           "an1010a84_162";
+            intResId      = R.drawable.an1010a84_162;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a98_189).getConstantState())){
+            strZellenname =           "an0101a98_189";
+            intResId      = R.drawable.an0101a98_189;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a98_189).getConstantState())){
+            strZellenname =           "an1010a98_189";
+            intResId      = R.drawable.an1010a98_189;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a112_216).getConstantState())){
+            strZellenname =           "an0101a112_216";
+            intResId      = R.drawable.an0101a112_216;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a112_216).getConstantState())){
+            strZellenname =           "an1010a112_216";
+            intResId      = R.drawable.an1010a112_216;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a126_243).getConstantState())){
+            strZellenname =           "an0101a126_243";
+            intResId      = R.drawable.an0101a126_243;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a126_243).getConstantState())){
+            strZellenname =           "an1010a126_243";
+            intResId      = R.drawable.an1010a126_243;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an0101a140_27).getConstantState())){
+            strZellenname =           "an0101a140_27";
+            intResId      = R.drawable.an0101a140_27;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.an1010a140_27).getConstantState())){
+            strZellenname =           "an1010a140_27";
+            intResId      = R.drawable.an1010a140_27;
+        }
+
+        // ****
+        // "en"
+        // ****
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en2101a12_011).getConstantState())){
+            strZellenname =           "en2101a12_011";
+            intResId      = R.drawable.en2101a12_011;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en1210a12_011).getConstantState())){
+            strZellenname =           "en1210a12_011";
+            intResId      = R.drawable.en1210a12_011;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en0121a12_011).getConstantState())){
+            strZellenname =           "en0121a12_011";
+            intResId      = R.drawable.en0121a12_011;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en1012a12_011).getConstantState())){
+            strZellenname =           "en1012a12_011";
+            intResId      = R.drawable.en1012a12_011;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en2020a12_011).getConstantState())){
+            strZellenname =           "en2020a12_011";
+            intResId      = R.drawable.en2020a12_011;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en0202a12_011).getConstantState())){
+            strZellenname =           "en0202a12_011";
+            intResId      = R.drawable.en0202a12_011;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en2110a24_022).getConstantState())){
+            strZellenname =           "en2110a24_022";
+            intResId      = R.drawable.en2110a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en2011a24_022).getConstantState())){
+            strZellenname =           "en2011a24_022";
+            intResId      = R.drawable.en2011a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en1120a24_022).getConstantState())){
+            strZellenname =           "en1120a24_022";
+            intResId      = R.drawable.en1120a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en1021a24_022).getConstantState())){
+            strZellenname =           "en1021a24_022";
+            intResId      = R.drawable.en1021a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en1111a24_022).getConstantState())){
+            strZellenname =           "en1111a24_022";
+            intResId      = R.drawable.en1111a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en0211a24_022).getConstantState())){
+            strZellenname =           "en0211a24_022";
+            intResId      = R.drawable.en0211a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en1201a24_022).getConstantState())){
+            strZellenname =           "en1201a24_022";
+            intResId      = R.drawable.en1201a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en1102a24_022).getConstantState())){
+            strZellenname =           "en1102a24_022";
+            intResId      = R.drawable.en1102a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en0112a24_022).getConstantState())){
+            strZellenname =           "en0112a24_022";
+            intResId      = R.drawable.en0112a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.en1111b24_022).getConstantState())){
+            strZellenname =           "en1111b24_022";
+            intResId      = R.drawable.en1111b24_022;
+        }
+
+        // ****
+        // "in"
+        // ****
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.in1010a24_022).getConstantState())){
+            strZellenname =           "in1010a24_022";
+            intResId      = R.drawable.in1010a24_022;
+        }
+        if (drawable.getConstantState().equals(ContextCompat.getDrawable(getBaseContext(), R.drawable.in0101a24_022).getConstantState())){
+            strZellenname =           "in0101a24_022";
+            intResId      = R.drawable.in0101a24_022;
+        }
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
         SharedPreferences.Editor prefEditor = prefs.edit();
 
-        prefEditor.putString("Strukturformel", strZellenname);
+        prefEditor.putString("Strukturformel-Zellenname", strZellenname);
+        prefEditor.putInt   ("Strukturformel-ResId"     , intResId);
+
         prefEditor.apply();
 
 

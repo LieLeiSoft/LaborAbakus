f64d2592692acc1e9aba61c7b05ff9b01068cd72
Frank <LieLei.Soft@gmail.com>
2020-03-03 17:21:35 +0100

    Generator überarbeitet: am Anfang Toast und Plausi-Prüfungen eingebaut

--- app/src/main/java/de/laborabakus/Org_Generator_Activity.java
+++ app/src/main/java/de/laborabakus/Org_Generator_Activity.java
@@ -5,24 +5,23 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
-
+import android.widget.Toast;
 
 public class Org_Generator_Activity extends Activity {
 
-    // String[][] arrGitter = new String[12][6];
+    String[][] arrGitter = new String[12][6]; // 12 Zeilen (0..11), 6 Spalten (0..5)
 
     String strZellenname;
-    String strStrukturformel;
     String strZeile;
-    String strZelle;
     String strSpalte;
     int intCounter = 0;
     int resIdFeld;
-    int intZelle;
 
-	@Override
+    @Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 	    super.onCreate(savedInstanceState);
@@ -30,16 +29,28 @@ public class Org_Generator_Activity extends Activity {
 
     	// Activity registrieren, damit sie sp�ter an zentraler Stelle (Hauptmenue) geschlossen werden kann
 	    ActivityRegistry.register(this);
-
 	} // onCreate
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // alle Activities AUSSER "HauptmenueActivity" schliessen
+        ActivityRegistry.finishAll("HauptmenueActivity");
+    } // onDestroy
 
     @Override
     public void onResume()
     {
         super.onResume();
 
-
+        // Toast anzeigen
+        if(resIdFeld == 0) {
+            String text = "\n   Zum Starten beliebige Zelle antippen.   \n";
+            Toast Meldung = Toast.makeText(this, text, Toast.LENGTH_LONG);
+            Meldung.setGravity(Gravity.BOTTOM, 0, 0);
+            Meldung.show();
+        }
 
         if(resIdFeld != 0) // Wird abgefangen, wenn beim ersten Mal resID =0
         {
@@ -52,43 +63,181 @@ public class Org_Generator_Activity extends Activity {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 
             int intResId = prefs.getInt("Strukturformel-ResId", 0);
+            if(intResId != 0) {
+                ibt = (ImageButton) findViewById(resIdFeld);
 
-            ibt = (ImageButton) findViewById(resIdFeld);
+                // strZellenname aus Preferences lesen ("Strukturformel-Zellenname")
+                String strZellenname = prefs.getString("Strukturformel-Zellenname", "");
 
-            // strZellenname aus Preferences lesen ("Strukturformel-Zellenname")
-            // Zeile/Spalte aus Feldnamen der aktuellen Zelle (resIdFeld) ermitteln (=strZeile/strSpalte)
-            // arrGitter[Int(strZeile)][Int(strSpalte)] = strZellenname
 
-            ibt.setImageResource(intResId);
+                int intZeile  = Integer.parseInt(strZeile ) - 1;
+                int intSpalte = Integer.parseInt(strSpalte) - 1;
+
+                arrGitter[intZeile][intSpalte] = strZellenname;
+
+                if (checkGitter() == true) {
+                    ibt.setImageResource(intResId);
+                }
+                else {
+                    arrGitter[intZeile][intSpalte]= null;
+                }
+
+            }
         }
     } // onResume
-
 
     @Override
     public void onPause() {
         super.onPause();
-
     } // onPause
 
     public void btnFeld(View v)
     {
-        strZellenname = getResources().getResourceEntryName(v.getId());  // Der ID Zellenname wird in einen String umgewandelt
+        resIdFeld = v.getId();
+        strZellenname = getResources().getResourceEntryName(resIdFeld);  // Zellenname (String) zur ID ermitteln
 
-        resIdFeld = getResources().getIdentifier(strZellenname, "id", getPackageName());
-
-        strZeile = strZellenname.substring(8,10);   // Beispiel: ibtFeld_115 => 11
+        // Zeile/Spalte aus Feldnamen der aktuellen Zelle (resIdFeld) ermitteln (=strZeile/strSpalte)
+        strZeile  = strZellenname.substring( 8,10);   // Beispiel: ibtFeld_115 => 11
         strSpalte = strZellenname.substring(10,11);   // Beispiel: ibtFeld_115 => 5
 
-        intCounter ++;
-
+        intCounter++;
 
         Intent myIntent = new Intent(Org_Generator_Activity.this, Org_Strukturformeln_Activity.class);
         myIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivity(myIntent);
+    } // btnFeld
 
+    public boolean checkGitter()
+    {
+        int arrBindung[] = new int[4];
+        int intZeile_max;
+        int intSpalte_max;
+        int intBindung_Nachbarzelle;
+        String strZellinhalt;
+        String strMsg;
 
-    }
+        intZeile_max = arrGitter.length - 1;
+        for (int intZeile = 0; intZeile < arrGitter.length; intZeile++) {
+            for (int intSpalte = 0; intSpalte < arrGitter[intZeile].length; intSpalte++) {
+                intSpalte_max = arrGitter[intZeile].length - 1;
+                if (arrGitter[intZeile][intSpalte] != null) {
+                    strZellinhalt = arrGitter[intZeile][intSpalte]; // Bsp.: an1010a56_108
+                    // Bindungseigenschaften in Array speichern
+                    for (int b = 0; b < 4; b++) {
+                        int intPos = 2 + b;
+                        arrBindung[b] = Integer.parseInt(strZellinhalt.substring(intPos, intPos+1));
+                    }
+                    Log.i("Org_Generator_Activity", "Zeile: " + intZeile + ", Spalte: " + intSpalte + ": " + arrGitter[intZeile][intSpalte]);
 
+                    strMsg = "";
+
+                    if ((arrBindung[0] > 0) && (intZeile == 0)) {
+                        // Bindung auf 12 Uhr in oberster Zeile nicht zulässig
+                        strMsg = "Bindung auf 12 Uhr in oberster Zeile nicht zulässig!";
+                    }
+                    else if ((arrBindung[1] > 0) && (intSpalte == intSpalte_max)) {
+                        // Bindung auf 3 Uhr in letzter Spalte nicht zulässig
+                        strMsg = "Bindung auf 3 Uhr in letzter Spalte nicht zulässig!";
+                    }
+                    else if ((arrBindung[2] > 0) && (intZeile == intZeile_max)) {
+                        // Bindung auf 6 Uhr in letzter Zeile nicht zulässig
+                        strMsg = "Bindung auf 6 Uhr in letzter Zeile nicht zulässig!";
+                    }
+                    else if ((arrBindung[3] > 0) && (intSpalte == 0)) {
+                        // Bindung auf 9 Uhr in erster Spalte nicht zulässig
+                        strMsg = "Bindung auf 9 Uhr in erster Spalte nicht zulässig!";
+                    }
+                    else {
+                        // prüfen, ob Bindungseigenschaften des aktuellen Elements mit denen der 4 umliegenenden Zellen
+                        // (12, 3, 6 und 9 Uhr) zusammenpassen
+                        // 12 Uhr-Bindung prüfen
+                        int b = 0;
+                        if (intZeile > 0) {
+                            if (arrGitter[intZeile - 1][intSpalte] != null) {
+                                // Nachbarzelle ist gefüllt
+                                strZellinhalt = arrGitter[intZeile - 1][intSpalte]; // Bsp.: an1010a56_108
+                                // 6 Uhr-Bindung der Zelle eine Zeile über dem aktuellen Element prüfen
+                                int intPos = 4;
+                                intBindung_Nachbarzelle = Integer.parseInt(strZellinhalt.substring(intPos, intPos + 1));
+                                if ((arrBindung[b] > 0) && (arrBindung[b] != intBindung_Nachbarzelle)) {
+                                    strMsg = "Bindung auf 12 Uhr passt nicht zur Nachbarzelle!";
+                                } else if ((arrBindung[b] == 0) && (intBindung_Nachbarzelle > 0)) {
+                                    strMsg = "Fehlende Bindung auf 12 Uhr passt nicht zur Nachbarzelle!";
+                                }
+                            }
+                        }
+
+                        if (strMsg.equals("")) {
+                            // 3 Uhr-Bindung prüfen
+                            b = 1;
+                            if (intSpalte < intSpalte_max) {
+                                if (arrGitter[intZeile][intSpalte + 1] != null) {
+                                    // Nachbarzelle ist gefüllt
+                                    strZellinhalt = arrGitter[intZeile][intSpalte + 1]; // Bsp.: an1010a56_108
+                                    // 9 Uhr-Bindung der Zelle eine Spalte neben dem aktuellen Element prüfen
+                                    int intPos = 5;
+                                    intBindung_Nachbarzelle = Integer.parseInt(strZellinhalt.substring(intPos, intPos + 1));
+                                    if ((arrBindung[b] > 0) && (arrBindung[b] != intBindung_Nachbarzelle)) {
+                                        strMsg = "Bindung auf 3 Uhr passt nicht zur Nachbarzelle!";
+                                    } else if ((arrBindung[b] == 0) && (intBindung_Nachbarzelle > 0)) {
+                                        strMsg = "Fehlende Bindung auf 3 Uhr passt nicht zur Nachbarzelle!";
+                                    }
+                                }
+                            }
+                        }
+
+                        if (strMsg.equals("")) {
+                            // 6 Uhr-Bindung prüfen
+                            b = 2;
+                            if (intZeile < intZeile_max) {
+                                if (arrGitter[intZeile + 1][intSpalte] != null) {
+                                    // Nachbarzelle ist gefüllt
+                                    strZellinhalt = arrGitter[intZeile + 1][intSpalte]; // Bsp.: an1010a56_108
+                                    // 12 Uhr-Bindung der Zelle eine Spalte unter dem aktuellen Element prüfen
+                                    int intPos = 2;
+                                    intBindung_Nachbarzelle = Integer.parseInt(strZellinhalt.substring(intPos, intPos + 1));
+                                    if ((arrBindung[b] > 0) && (arrBindung[b] != intBindung_Nachbarzelle)) {
+                                        strMsg = "Bindung auf 6 Uhr passt nicht zur Nachbarzelle!";
+                                    } else if ((arrBindung[b] == 0) && (intBindung_Nachbarzelle > 0)) {
+                                        strMsg = "Fehlende Bindung auf 6 Uhr passt nicht zur Nachbarzelle!";
+                                    }
+                                }
+                            }
+                        }
+
+                        if (strMsg.equals("")) {
+                            // 9 Uhr-Bindung prüfen
+                            b = 3;
+                            if (intSpalte > 0) {
+                                if (arrGitter[intZeile][intSpalte - 1] != null) {
+                                    // Nachbarzelle ist gefüllt
+                                    strZellinhalt = arrGitter[intZeile][intSpalte - 1]; // Bsp.: an1010a56_108
+                                    // 3 Uhr-Bindung der Zelle eine Spalte unter dem aktuellen Element prüfen
+                                    int intPos = 3;
+                                    intBindung_Nachbarzelle = Integer.parseInt(strZellinhalt.substring(intPos, intPos + 1));
+                                    if ((arrBindung[b] > 0) && (arrBindung[b] != intBindung_Nachbarzelle)) {
+                                        strMsg = "Bindung auf 9 Uhr passt nicht zur Nachbarzelle!";
+                                    } else if ((arrBindung[b] == 0) && (intBindung_Nachbarzelle > 0)) {
+                                        strMsg = "Fehlende Bindung auf 9 Uhr passt nicht zur Nachbarzelle!";
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (strMsg != "") {
+                        strMsg = "\n   " + strMsg + "   \n";
+                        Toast Meldung = Toast.makeText(this, strMsg, Toast.LENGTH_LONG);
+                        Meldung.setGravity(Gravity.BOTTOM, 0, 0);
+                        Meldung.show();
+                        return false;
+                    }
+                } // if (arrGitter[intZeile][intSpalte] != null)
+            } // for (int intZeile = 0; intZeile < arrGitter.length; intZeile++)
+        } // for (int intZeile = 0; intZeile < arrGitter.length; intZeile++)
+
+        return true;
+    } // checkGitter
 
 } // class Org_Generator
 
--- app/src/main/java/de/laborabakus/Org_Strukturformeln_Activity.java
+++ app/src/main/java/de/laborabakus/Org_Strukturformeln_Activity.java
@@ -2,8 +2,8 @@ package de.laborabakus;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
@@ -13,10 +13,11 @@ import android.widget.ImageButton;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Org_Strukturformeln_Activity extends Activity {
+    // Stringlist 'names', die später die Namen der Bilddateien (z.B. "co1111a12_011", "hh1000a1_008") enthalten soll
+    static List<String> names = new ArrayList<String>();
 
     @Override
 	public void onCreate(Bundle savedInstanceState)
@@ -24,25 +25,71 @@ public class Org_Strukturformeln_Activity extends Activity {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.org_strukturformeln);
 
-    	// Activity registrieren, damit sie sp�ter an zentraler Stelle (Hauptmenue) geschlossen werden kann
-	    ActivityRegistry.register(this);
+        // Activity registrieren, damit sie sp�ter an zentraler Stelle (Hauptmenue) geschlossen werden kann
+        ActivityRegistry.register(this);
 
+        ErstelleListe();
 	} // onCreate
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        names.clear();
+    } // onDestroy
 
     @Override
     public void onResume()
     {
         super.onResume();
-
     } // onResume
-
 
     @Override
     public void onPause() {
         super.onPause();
-
     } // onPause
+
+    private void ErstelleListe()
+    {
+        // Feldliste definieren, die alle Grafik-Objekte (drawables) des Projekts enthält
+        Field[] fields =  R.drawable.class.getFields();
+
+        /*
+            Die Namen der Bilddateien müssen folgen Aufbau haben:
+            xx     = Typ (z.B. an = Alkane)
+            1234   = Bindungseigenschaften (1. Ziffer: Bindung 12:00 Uhr, 2. Ziffer: Bindung 15:00 Uhr, usw.)
+            x      = Variante (mehrere Möglichkeiten, a = Standard)
+            12_345 = Molmasse, der Unterstrich ist gleich Komma (z.B. 13_019 = 13,019 g/mol)
+
+            Beispiele:
+            Kohlenstoff: "co1111a12_011"
+            Wasserstoff: "hh1000a1_008"
+        */
+
+        /*
+            Hier wird ein "Regulärer Ausdruck" ("Pattern") gebildet, um die gewünschten Grafik-Objekte
+            (z.B. "co1111a12_011", "hh1000a1_008") in der Feldliste zu finden.
+
+            Der "Reguläre Ausdruck" setzt sich aus verschiedenen Platzhaltern zusammen:
+            .*          : beliebiges Zeichen, beliebig oft
+            [a-zA-Z]{2} : 2 Buchstaben (Groß-/Kleinschreibung egal)
+            \d{4}       : 4 Ziffern
+            [a-zA-Z]    : 1 Buchstabe (Groß-/Kleinschreibung egal)
+            \d+         : Ziffer, beliebig oft
+            [_]         : Unterstrich
+            \d+         : Ziffer, beliebig oft
+
+            Info zu "Regulären Ausdrücken": http://openbook.rheinwerk-verlag.de/javainsel9/javainsel_04_007.htm#mj26fc5cf60311afbddd72295cdd646a48
+        */
+        Pattern p = Pattern.compile(".*[a-zA-Z]{2}\\d{4}[a-zA-Z]\\d+[_]\\d+");
+
+        // Die Feldliste wird mit dem "Regulärer Ausdruck" ("Pattern") durchsucht.
+        // Jeder gefunden Eintrag wird in die Stringlist 'names' eingetragen
+        for (Field field : fields) {
+            if (p.matcher(field.getName()).matches())
+                names.add(field.getName());
+        }
+    }
 
     public void btnZelle(View v)
     {
@@ -50,41 +97,6 @@ public class Org_Strukturformeln_Activity extends Activity {
         int intResId = 0;
         int i = 0;
         boolean bBildGefunden = false;
-
-        // Die Namen der Bilddateien sind wie folgt aufgebaut:
-        // xx = Typ (z.B. an = Alkane)
-        // 1234 = Bindungseigenschaften 1.Ziffer - Bindung 12:00 Uhr, 2.Ziffer - Bindung 15:00 Uhr, usw.
-        // x = Variante (mehrere Möglichkeiten, a = Standard)
-        // 12_345 = Molmasse, der Unterstrich ist gleich Komma(z.B. 13_019 = 13,019 g/mol)
-
-        /*
-            Doku:
-
-                // ***********
-                // Kohlenstoff
-                // ***********
-                "co1111a12_011"
-                // ***********
-                // Wasserstoff
-                // ***********
-                ,"hh1000a1_008", "hh0100a1_008", "hh0010a1_008", "hh0001a1_008"
-                // ********************
-                // -Cx-Hx- Alkylgruppen
-                // ********************
-                ,"an1101a13_019", "an1110a13_019", "an0111a13_019", "an1011a13_019", "an0101a14_027", "an1010a14_027", "an1000a15_035", "an0100a15_035", "an0010a15_035", "an0001a15_035", "an0101a28_054", "an1010a28_054", "an0101a42_081", "an1010a42_081", "an0101a56_108", "an1010a56_108", "an0101a70_135", "an1010a70_135", "an0101a84_162", "an1010a84_162", "an0101a98_189", "an1010a98_189", "an0101a112_216", "an1010a112_216", "an0101a126_243", "an1010a126_243", "an0101a140_27", "an1010a140_27"
-                // *********************
-                // -C=C- Doppelbindungen
-                // *********************
-                ,"en2101a12_011", "en1210a12_011", "en0121a12_011", "en1012a12_011", "en2020a12_011", "en0202a12_011", "en2110a24_022", "en2011a24_022", "en1120a24_022", "en1021a24_022", "en1111a24_022", "en0211a24_022", "en1201a24_022", "en1102a24_022", "en0112a24_022", "en1111b24_022"
-                // ***********************
-                // -C≡C- Dreifachbindungen
-                // ***********************
-                ,"in1010a24_022", "in0101a24_022"
-                // ***********************
-                // -OH Hydroxylgruppen
-                // ***********************
-                ,"hy0001a17_007", "hy0100a17_007"
-        */
 
         ImageButton btn = (ImageButton) findViewById(v.getId());
         Drawable drawable = btn.getDrawable();
@@ -101,28 +113,7 @@ public class Org_Strukturformeln_Activity extends Activity {
         // if (drawable.getConstantState().equals(getResources().getDrawable(R.drawable.hh0001a1_008).getConstantState())){
         // "getResources().getDrawable" ist seit Android 5.1 (API 22) veraltet (deprecated)
 
-        Field[] fields =  R.drawable.class.getFields();
-        List<String> names = new ArrayList<String>();
-
-        // Info zu "Regulären Ausdrücken": http://openbook.rheinwerk-verlag.de/javainsel9/javainsel_04_007.htm#mj26fc5cf60311afbddd72295cdd646a48
-        /*
-            Der "Reguläre Ausdruck" setzt sich aus verschiedenen Platzhaltern zusammen:
-            .*          : beliebiges Zeichen, beliebig oft
-            [a-zA-Z]{2} : 2 Buchstaben (Groß-/Kleinschreibung egal)
-            \d{4}       : 4 Ziffern
-            [a-zA-Z]    : 1 Buchstabe (Groß-/Kleinschreibung egal)
-            \d+         : Ziffer, beliebig oft
-            [_]         : Unterstrich
-            \d+         : Ziffer, beliebig oft
-        */
-        Pattern p = Pattern.compile(".*[a-zA-Z]{2}\\d{4}[a-zA-Z]\\d+[_]\\d+");
-
-        for (Field field : fields) {
-            if (p.matcher(field.getName()).matches())
-                names.add(field.getName());
-        }
-
-        // Lösung mit Stringlist
+        // Stringlist 'names' durchlaufen
         do
         {
             strZellenname = names.get(i);
@@ -139,7 +130,7 @@ public class Org_Strukturformeln_Activity extends Activity {
             SharedPreferences.Editor prefEditor = prefs.edit();
 
             prefEditor.putString("Strukturformel-Zellenname", strZellenname);
-            prefEditor.putInt("Strukturformel-ResId", intResId);
+            prefEditor.putInt   ("Strukturformel-ResId"     , intResId     );
 
             prefEditor.apply();
 

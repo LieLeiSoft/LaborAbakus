6fa6e200798f6df36278be1e15224cb2d934d51d
Ingo_Arbeit <lieleisoft.com>
2020-12-05 17:30:08 +0100

    Endkonzentration_Activity - Programierung der Buttons: fest/flüssig, ReinheitEinheit und EinmassEinheit

--- app/src/main/java/de/laborabakus/Endkonzentration_Activity.java
+++ app/src/main/java/de/laborabakus/Endkonzentration_Activity.java
@@ -2,18 +2,27 @@ package de.laborabakus;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Endkonzentration_Activity extends Activity {
 
+    private static final String TAG = "";
     EditText et;
+    TextView tv;
+    String strAuswahl;
+    String strAuswahl2;
+    String strAuswahl3;
     
 	/** wird ausgef�hrt, wenn Activicty erstellt wird */
 	@Override
@@ -44,8 +53,28 @@ public class Endkonzentration_Activity extends Activity {
     protected void onResume()
     {
         super.onResume();
+/*
+        String strAuswahl;
+        TextView tv;
 
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 
+        strAuswahl = prefs.getString("Reinheit_Konz_solid_liquid", "fest");
+
+        if (strAuswahl.equals("fest"))
+        {
+            strAuswahl = getResources().getString(R.string.aktuell_fest);
+            tv = (TextView) findViewById(R.id.btnFestFluessig);
+            tv.setText(strAuswahl);
+        }
+
+        if (strAuswahl.equals("flüssig"))
+        {
+            strAuswahl = getResources().getString(R.string.aktuell_fluessig);
+            tv = (TextView) findViewById(R.id.btnFestFluessig);
+            tv.setText(strAuswahl);
+        }
+*/
     } // onResume
 
     @Override
@@ -55,7 +84,143 @@ public class Endkonzentration_Activity extends Activity {
 
     } // onPause
 
+    /********************************************
+     ************** Button fest / flüssig *******
+     ********************************************/
 
+    public void btnOnClickFestFluessig (View v)
+    {
+        // ********************************************
+        // *** Wechsel der Anzeige "fest" "flüssig" ***
+        // ********************************************
+
+        tv = (TextView) findViewById(R.id.btnFestFluessig);
+        strAuswahl = tv.getText().toString();
+
+        if (strAuswahl.equals("fest"))
+        {
+            tv = (TextView) findViewById(R.id.btnFestFluessig);
+            tv.setText(R.string.aktuell_fluessig);
+
+            tv = (TextView) findViewById(R.id.btnEinheitReinheit);
+            tv.setText(R.string.mg_ml);
+
+            tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+            strAuswahl3 = tv.getText().toString();
+
+            if (strAuswahl3.equals("mg"))
+            {
+                tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+                tv.setText(R.string.ul);
+            }
+
+            if (strAuswahl3.equals("g"))
+            {
+                tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+                tv.setText(R.string.ml);
+            }
+        }
+
+        if (strAuswahl.equals("flüssig"))
+        {
+            tv = (TextView) findViewById(R.id.btnFestFluessig);
+            tv.setText(R.string.aktuell_fest);
+
+            tv = (TextView) findViewById(R.id.btnEinheitReinheit);
+            tv.setText(R.string.mg_g);
+
+            tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+            strAuswahl3 = tv.getText().toString();
+
+            if (strAuswahl3.equals("µl"))
+            {
+                tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+                tv.setText(R.string.mg);
+            }
+
+            if (strAuswahl3.equals("ml"))
+            {
+                tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+                tv.setText(R.string.g);
+            }
+        }
+    }
+
+    /********************************************
+     ************** Button % / mg/ml ************
+     ********************************************/
+
+    public void btnOnClickEinheitReinheit (View v)
+    {
+        // ********************************************
+        // *** Wechsel der Anzeige "%" "mg/ml" ********
+        // ********************************************
+
+        tv = (TextView) findViewById(R.id.btnFestFluessig);
+        strAuswahl = tv.getText().toString();
+
+        tv = (TextView) findViewById(R.id.btnEinheitReinheit);
+        strAuswahl2 = tv.getText().toString();
+
+        if (strAuswahl2.equals("%"))
+        {
+            if (strAuswahl.equals("fest"))
+            {
+                tv = (TextView) findViewById(R.id.btnEinheitReinheit);
+                tv.setText(R.string.mg_g);
+            }
+            if (strAuswahl.equals("flüssig"))
+            {
+                tv = (TextView) findViewById(R.id.btnEinheitReinheit);
+                tv.setText(R.string.mg_ml);
+            }
+        }
+
+        if (strAuswahl2.equals("mg/ml") || strAuswahl2.equals("mg/g"))
+        {
+            tv = (TextView) findViewById(R.id.btnEinheitReinheit);
+            tv.setText(R.string.Prozent);
+        }
+    }
+
+    /********************************************
+     ************** Button EinheitEinmass *******
+     ********************************************/
+
+    public void btnOnClickEinheitEinmass (View v)
+    {
+        // *************************************************************
+        // *** Wechsel der Anzeige "µg"/"µl" "mg"/"ml"  "g"/"l" ********
+        // *************************************************************
+
+        tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+        strAuswahl3 = tv.getText().toString();
+
+        tv = (TextView) findViewById(R.id.btnFestFluessig);
+        strAuswahl = tv.getText().toString();
+
+        if (strAuswahl.equals("fest") && strAuswahl3.equals("mg"))
+        {
+            tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+            tv.setText(R.string.g);
+        }
+        if (strAuswahl.equals("fest") && strAuswahl3.equals("g"))
+        {
+            tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+            tv.setText(R.string.mg);
+        }
+
+        if (strAuswahl.equals("flüssig") && strAuswahl3.equals("µl"))
+        {
+            tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+            tv.setText(R.string.ml);
+        }
+        if (strAuswahl.equals("flüssig") && strAuswahl3.equals("ml"))
+        {
+            tv = (TextView) findViewById(R.id.btnEinheitEinmass);
+            tv.setText(R.string.ul);
+        }
+    }
 
 	/********************************************
 	 ************** Menue Button ****************
